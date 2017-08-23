604fd3a49754b935a8b99688215688c392f1943a
David Dassau <todieahero@hotmail.com>
2017-08-22 21:13:00 -0500

    made new json for dogs

--- main.js
+++ main.js
@@ -21,4 +21,45 @@ function writeToDom(catString){
 }
 
 
-domString(cats);
+function executeThisCodeAfterFileLoads() {
+	var data = JSON.parse(this.responseText);
+	domString(data.cats);
+}
+
+function executeThisCodeAfterFileLoads2() {
+	var data = JSON.parse(this.responseText);
+	domString(data.dogs);
+}
+
+function executeThisCodeIfFileErrors() {
+	console.log("Shit broke");
+}
+
+function executeThisCodeIfFileErrors2() {
+	console.log("Shit broke");
+}
+
+
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener("load", executeThisCodeAfterFileLoads);
+myRequest.addEventListener("error", executeThisCodeIfFileErrors);
+myRequest.open("GET", "cats.json");
+myRequest.send();
+
+
+var myRequest2 = new XMLHttpRequest();
+myRequest2.addEventListener("load", executeThisCodeAfterFileLoads2);
+myRequest2.addEventListener("error", executeThisCodeIfFileErrors2);
+myRequest2.open("GET", "dogs.json");
+myRequest2.send();
+
+
+
+
+
+
+
+
+
+
+
